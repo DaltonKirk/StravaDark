876f907623a087f09a29f14f23002f8b5153e567
Dalton Kirk <daltonkirk5@gmail.com>
2021-04-22 23:13:14 +0100

    added sass lint gulp task and fixed some lint errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
+var sassLint = require('gulp-sass-lint');
 
 sass.compiler = require('node-sass');
- 
-gulp.task('sass', function () {
+
+gulp.task('sass:lint', function () {
   return gulp.src('./src/scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(sassLint())
+  .pipe(sassLint.format())
+  .pipe(sassLint.failOnError());
+});
+
+gulp.task('sass', function () {
+  return gulp
+    .src('./src/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(autoprefixer('last 2 versions'))
     .pipe(rename('darkstrava.min.css'))
     .pipe(gulp.dest('./dist/css'));
 });
- 
+
 gulp.task('sass:watch', function () {
-  gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('./src/scss/**/*.scss', gulp.series('sass:lint', 'sass'));
 });
